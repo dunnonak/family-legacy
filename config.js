c567d2e34af6dfea5a2f4312a7cab871aebb212a
Askia Dunnon <askia.dunnon@cuanschutz.edu>
2025-07-06 17:59:25 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// This file stores the administrator password.
+// IMPORTANT: Change this password to something secure and known only to your family.
+// IMPORTANT: Do NOT share this file publicly or commit it to a public code repository.
+
+window.ADMIN_PASSWORD = 'ourlegacy';
